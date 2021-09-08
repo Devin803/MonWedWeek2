41a4846922b7bb314944af68cc9b87bd9c735cc3
devin803 <Dinocencio@student.bridgew.edu>
2021-09-08 14:42:05 -0400

    new example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	
+	    var reader = new Scanner(System.in); //
+        System.out.print("How old are you:"); //prompting user to enter their age using system.out.print
+        var age = reader.nextFloat(); //setting the variable age
+        var dogYears = age*7; //setting the variable dogYears by multiplying age by 7
+        System.out.println("Wow you are " + age+" that is "+ dogYears+" in dog years"); //calling for the two variables age and dog years
+
     }
 }
